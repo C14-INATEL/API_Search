edb7e72bc731c4f35618db1622a84472edb01d47
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-20 23:01:01 -0300

    🚧  builting User class with attributes and encapsulation

---
+++ src/main/java/com/api_search/project/user/User.java
@@ -0,0 +1,12 @@
+package com.api_search.project.user;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String token;
+    private Date date;
+}
