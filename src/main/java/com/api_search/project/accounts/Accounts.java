53d3dd440035ac55e1af667c2292fef7d5bc7b12
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2026-03-20 23:29:25 -0300

    :construction: building account class with encapsulation

---
+++ src/main/java/com/api_search/project/accounts/Accounts.java
@@ -0,0 +1,10 @@
+package com.api_search.project.accounts;
+
+public class Accounts {
+    private int ID;
+    private int user_ID;
+    private String address;
+    private String description;
+    private String status;
+    private String password_hash;
+}
