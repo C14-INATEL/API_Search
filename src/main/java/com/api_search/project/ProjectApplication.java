e46324b2ca68c9317a49a0a3dd19908b5c80d6e1
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-23 20:05:40 -0300

    ⬆️ upgrade dependencies dev-tools and spring boot starter

--- src/main/java/com/api_search/project/ProjectApplication.java
+++ src/main/java/com/api_search/project/ProjectApplication.java
@@ -3,11 +3,12 @@ package com.api_search.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-
 }
