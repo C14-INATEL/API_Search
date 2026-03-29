e6c1cc6b0a3c6ef067840ce9140010b24cda0443
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-29 15:42:20 -0300

    🚧  creating new interface

--- src/main/java/com/api_search/project/service/UserService.java
+++ src/main/java/com/api_search/project/service/UserService.java
@@ -1,4 +1,33 @@
 package com.api_search.project.service;
 
+import com.api_search.project.entity.User;
+import com.api_search.project.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    // constructor
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void save(User user){
+        userRepository.save(user);
+    }
+
+    public User searchById(Integer id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> searchAll(){
+        return userRepository.findAll();
+    }
+
+    public boolean existsByid(Integer id) {
+        return userRepository.existsById(id);
+    }
 }
