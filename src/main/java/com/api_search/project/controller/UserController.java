cfb8c518f61090f4237dc068b01d4c75db640a41
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-29 14:23:21 -0300

    🚧  setting routes of api

--- src/main/java/com/api_search/project/controller/UserController.java
+++ src/main/java/com/api_search/project/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.api_search.project.controller;
 
+import com.api_search.project.entity.User;
+import com.api_search.project.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private UserService userService;
+
+    //constructor
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void save(@RequestBody User user){
+        userService.save(user);
+    }
+
+    @GetMapping("{id}")
+    public User searchById(@PathVariable Integer id){
+        return userService.searchById(id);
+    }
+
+    @GetMapping
+    public List<User> search(){
+        return userService.searchAll();
+    }
+
 }
