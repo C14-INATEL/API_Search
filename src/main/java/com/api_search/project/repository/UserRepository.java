aa984b05f3b6bc8ca9ddc7aebf3d5a59189a2c88
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-29 15:41:47 -0300

    🚧  creating new interface

--- src/main/java/com/api_search/project/repository/UserRepository.java
+++ src/main/java/com/api_search/project/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.api_search.project.repository;
 
-public interface UsersRepository {
+import com.api_search.project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
