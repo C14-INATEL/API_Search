2d8398f8fdf6560acc4b4b964bbb46528e9c3352
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-29 22:05:47 -0300

    ⬆️ update getters and setter

--- src/main/java/com/api_search/project/entity/Accounts.java
+++ src/main/java/com/api_search/project/entity/Accounts.java
@@ -7,43 +7,68 @@ import jakarta.persistence.*;
 import org.springframework.cglib.core.Local;
 
 @Entity
+@Table(name = "accounts_monitored")
 public class Accounts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer account_ID;
+    private String description;
+    private String password_hash;
+
     @Column(name = "address_to_in")
     private String address;
 
-    private String description;
-
     @Column(name = "status_to_in")
     private String status;
-    private String password_hash;
 
     // Getters and Setters
 
-    public Integer getId() {return id;}
+    public Integer getId() {
+        return id;
+    }
 
-    public void setId(Integer id) {this.id = id;}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public Integer getAccount_ID() {return account_ID;}
+    public Integer getAccount_ID() {
+        return account_ID;
+    }
 
-    public void setAccount_ID(Integer account_ID) {this.account_ID = account_ID;}
+    public void setAccount_ID(Integer account_ID) {
+        this.account_ID = account_ID;
+    }
 
-    public String getAddress() {return address;}
+    public String getAddress() {
+        return address;
+    }
 
-    public void setAddress(String address) {this.address = address;}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-    public String getDescription() {return description;}
+    public String getDescription() {
+        return description;
+    }
 
-    public void setDescription(String description) {this.description = description;}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public String getStatus() {return status;}
+    public String getStatus() {
+        return status;
+    }
 
-    public void setStatus(String status) {this.status = status;}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-    public String getPassword_hash() {return password_hash;}
+    public String getPassword_hash() {
+        return password_hash;
+    }
 
-    public void setPassword_hash(String password_hash) {this.password_hash = password_hash;}
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
 }
