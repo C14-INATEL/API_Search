731234733fcec1f50d795d72e04f4797c8418fe8
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-29 22:04:58 -0300

    ⬆️ update routes

--- src/main/java/com/api_search/project/controller/AccountsController.java
+++ src/main/java/com/api_search/project/controller/AccountsController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
-public class AccountsController {
+public class AccountsController{
     private AccountsService accountsService;
 
     // Construtor
