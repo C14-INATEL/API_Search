15175b5d96c1eb83ec7740b0ea98099bdd7a2362
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-03-31 23:14:46 -0300

    ⬆️ update user controller fixed get and set in method update

--- src/main/java/com/api_search/project/controller/UserController.java
+++ src/main/java/com/api_search/project/controller/UserController.java
@@ -59,13 +59,18 @@ public class UserController {
         user_update.setEmail(user.getEmail());
         return userService.saveObject(user_update);
     }
-
     @PutMapping("/{user_id}/update_passowrd")
+    public User updatePassword(@PathVariable Integer id, @RequestBody User user) {
+        User user_update = userService.searchById(id);
+        user_update.setPassword(user.getPassword());
+        return userService.saveObject(user_update);
+    }
+
+    @PutMapping("/{user_id}/update_token")
     public User updateToken(@PathVariable Integer id, @RequestBody User user) {
         User user_update = userService.searchById(id);
         user_update.setToken(user.getToken());
         return userService.saveObject(user_update);
     }
-
 
 }
