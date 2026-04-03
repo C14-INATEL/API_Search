2a5cc3160afa1201b859ab749ff34c834fde99e2
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-04-03 18:05:23 -0300

    ➕ add new dependeces, mockito and junit

---
+++ src/test/java/com/api_search/project/service/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.api_search.project.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void saveObject() {
+    }
+
+    @Test
+    void searchById() {
+    }
+
+    @Test
+    void searchAll() {
+    }
+
+    @Test
+    void existsByid() {
+    }
+
+    @Test
+    void deleteByid() {
+    }
+
+    @Test
+    void deleteALL() {
+    }
+}
