7364ec9042829edd843a567d4a430f23945f700e
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-04-02 22:04:20 -0300

    🐛 resolve bug in object injection

--- src/main/java/com/api_search/project/controller/UserController.java
+++ src/main/java/com/api_search/project/controller/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
         userService.deleteALL();
     }
 
-    @PutMapping("/{user_id}/update_name")
+    @PutMapping("/{user_id}/updateNname")
     public User updateName(@PathVariable Integer id, @RequestBody User user) {
         User user_update = userService.searchById(id);
         user_update.setName(user.getName());
