1eea2ca2081df46859d0a20abe1c21fb2b640515
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-04-04 13:42:10 -0300

    👷 create dto class and interface to use query in sql to combine tables

--- src/main/java/com/api_search/project/dto/UserDashboardDTO.java
+++ src/main/java/com/api_search/project/dto/UserDashboardDTO.java
@@ -1,4 +1,26 @@
 package com.api_search.project.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface UserDashboardDTO {
+    Integer getUserId();
+    String getUsuario();
+    String getEmailUsuario();
+
+    Integer getAccountId();
+    String getEmailMonitorado();
+    String getContaMonitorada();
+    String getRiskStatus();
+
+    String getEmailVazado();
+
+    Integer getLeakId();
+    String getSiteVazado();
+    LocalDate getDateOccurrence();
+    Long getTotalRegistros();
+
+    Integer getAlertId();
+    String getRiskLevel();
+    LocalDateTime getDateAlert();
 }
