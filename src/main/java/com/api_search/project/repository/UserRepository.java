e01a0b859f985b3c15704f308c0fafc76223aa51
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-04-04 14:45:59 -0300

    👷 build complete query to the dasboard

--- src/main/java/com/api_search/project/repository/UserRepository.java
+++ src/main/java/com/api_search/project/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.api_search.project.repository;
 
+import com.api_search.project.dto.UserDashboardDTO;
 import com.api_search.project.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
