1d950a28995d4a5a4bf3340312274243fbee1d50
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-04-05 17:44:01 -0300

    🚧 build new class that help uncouple, performance, answer controler and protecd data, this help also not expose the entity

--- src/main/java/com/api_search/project/dto/LeakResponse.java
+++ src/main/java/com/api_search/project/dto/LeakResponse.java
@@ -1,4 +1,49 @@
 package com.api_search.project.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LeakResponse {
+
+    private boolean success;
+    private List<Source> sources;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public List<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<Source> sources) {
+        this.sources = sources;
+    }
+
+    public static class Source {
+
+        private String name;
+        private String date;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+    }
 }
