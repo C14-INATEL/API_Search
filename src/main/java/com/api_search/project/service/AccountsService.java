6aa457c2eed03041aef0f60638d0c888675e0a99
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-04-05 17:52:13 -0300

    ⬆️ rise method save and saveObject to store my method a password with hash

--- src/main/java/com/api_search/project/service/AccountsService.java
+++ src/main/java/com/api_search/project/service/AccountsService.java
@@ -3,6 +3,7 @@ package com.api_search.project.service;
 import com.api_search.project.entity.Accounts;
 import com.api_search.project.entity.Alert;
 import com.api_search.project.repository.AccountsRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +18,22 @@ public class AccountsService {
     }
 
     public void save(Accounts accounts){
+        String password;
+        String hash;
+
+        password = accounts.getPassword_hash();
+        hash = BCrypt.hashpw(password, BCrypt.gensalt());
+        accounts.setPassword_hash(hash);
         accountsRepository.save(accounts);
     }
 
     public Accounts saveObject(Accounts accounts) {
+        String password;
+        String hash;
+
+        password = accounts.getPassword_hash();
+        hash = BCrypt.hashpw(password, BCrypt.gensalt());
+        accounts.setPassword_hash(hash);
         return accountsRepository.save(accounts);
     }
 
