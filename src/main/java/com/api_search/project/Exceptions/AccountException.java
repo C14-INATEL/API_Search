ff89b360f4368d7c5952c80cf95b41a400dab58b
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2026-04-06 01:47:54 -0300

    :test_tube: Created AccountException

---
+++ src/main/java/com/api_search/project/Exceptions/AccountException.java
@@ -0,0 +1,7 @@
+package com.api_search.project.Exceptions;
+
+public class AccountException extends RuntimeException {
+    public AccountException(String message) {
+        super(message);
+    }
+}
