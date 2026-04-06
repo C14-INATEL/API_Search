1a3064ea281d07ca72a4b7ddb033c50e2c28c38c
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2026-04-06 01:45:58 -0300

    :test_tube:  Created Leakeds_emailException

---
+++ src/main/java/com/api_search/project/Exceptions/Leakeds_emailException.java
@@ -0,0 +1,7 @@
+package com.api_search.project.Exceptions;
+
+public class Leakeds_emailException extends RuntimeException {
+    public Leakeds_emailException(String message) {
+        super(message);
+    }
+}
