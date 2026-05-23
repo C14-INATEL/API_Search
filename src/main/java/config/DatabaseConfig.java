b89913d66386538e68cad40b0b87cacc752ab14b
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-23 17:07:56 -0300

    refactor: 🔥 remove datbase config, because it's being set in the properties as an environment variable.

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,4 +1,23 @@
 package config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
 public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource mainDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "audit.datasource")
+    public DataSource auditDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
