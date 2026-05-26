d100c7eb5c5ca0cbb805a6385487506ed21085c3
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-25 21:47:45 -0300

    feat: ✨ new resource, treatment of errors and throws of exception

--- src/main/java/com/api_search/project/controller/AccountsController.java
+++ src/main/java/com/api_search/project/controller/AccountsController.java
@@ -2,6 +2,7 @@ package com.api_search.project.controller;
 
 import com.api_search.project.client.FindByEmailClient;
 import com.api_search.project.entity.Accounts;
+import com.api_search.project.excepetion.AccountsExcept;
 import com.api_search.project.response.FindByEmailResponse;
 import com.api_search.project.service.AccountsService;
 import org.springframework.http.ResponseEntity;
@@ -23,50 +24,104 @@ public class AccountsController{
     }
 
     @PostMapping
-    public void save(@RequestBody Accounts Accounts){
-        accountsService.save(Accounts);}
-
-    @GetMapping("{id}")
-    public Accounts searchById(@PathVariable Integer id){
-        return accountsService.searchById(id);
+    public void save(@RequestBody Accounts Accounts) throws AccountsExcept {
+        try {
+            accountsService.save(Accounts);
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
 
+    @GetMapping("{id}")
+    public Accounts searchById(@PathVariable Integer id) throws AccountsExcept{
+        try {
+            return accountsService.searchById(id);
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
+        }
+
     @GetMapping
-    public List<Accounts> search(){
-        return accountsService.searchAll();
+    public List<Accounts> search() throws AccountsExcept{
+        try {
+            return accountsService.searchAll();
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
 
     @GetMapping("/user/{userId}")
-    public List<Accounts> searchAccountsByUserId(@PathVariable Integer userId){
-        return accountsService.searchAccountsByUser(userId);
+    public List<Accounts> searchAccountsByUserId(@PathVariable Integer userId) throws AccountsExcept {
+        try {
+            return accountsService.searchAccountsByUser(userId);
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
-
     @GetMapping("{id}/exist")
-    public boolean existById(@PathVariable Integer id){
-        return accountsService.existsByid(id);
+    public boolean existById(@PathVariable Integer id) throws AccountsExcept{
+        try {
+            return accountsService.existsByid(id);
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteaccountByID(@PathVariable Integer id){
-        accountsService.deleteByid(id);
+    public void deleteaccountByID(@PathVariable Integer id) throws AccountsExcept{
+        try {
+            accountsService.deleteByid(id);
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
 
     @DeleteMapping("user/{userId}")
-    public void deleteAllUserAccounts(@PathVariable Integer userId){
-        accountsService.deleteAllUserAccounts(userId);
-    }
+    public void deleteAllUserAccounts(@PathVariable Integer userId) throws AccountsExcept{
+        try {
+            accountsService.deleteAllUserAccounts(userId);
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
+        }
 
     @DeleteMapping
-    public void deleteALL(){
-        accountsService.deleteALL();
+    public void deleteALL() throws AccountsExcept{
+        try {
+            accountsService.deleteALL();
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
 
     @PostMapping("/accountMonitored/{userId}/{email}")
-    public ResponseEntity <String> accountMonitored(@PathVariable Integer userId, @PathVariable String email) {
-        findByEmailClient
-                .findByEmailResponseFlux(email)
-                .doOnNext(dto -> accountsService.saveFromResponseEmailWebClientHIBP(dto,email, userId))
-                .subscribe();
-        return ResponseEntity.ok("Account added successfully");
+    public ResponseEntity <String> accountMonitored(@PathVariable Integer userId, @PathVariable String email) throws AccountsExcept {
+        try {
+            findByEmailClient
+                    .findByEmailResponseFlux(email)
+                    .doOnNext(dto -> accountsService.saveFromResponseEmailWebClientHIBP(dto, email, userId))
+                    .subscribe();
+            return ResponseEntity.ok("Account added successfully");
+        }
+        catch (Exception e)
+        {
+            throw new AccountsExcept(e.getMessage());
+        }
     }
 }
