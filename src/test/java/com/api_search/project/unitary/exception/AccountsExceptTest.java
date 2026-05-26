d34a46fc251ad745b560b0c5a67b6cf064fa44aa
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-25 22:03:20 -0300

    test: ✅ create new test of class

--- src/test/java/com/api_search/project/unitary/exception/AccountsExceptTest.java
+++ src/test/java/com/api_search/project/unitary/exception/AccountsExceptTest.java
@@ -1,5 +1,23 @@
-package com.api_search.project.exception;
+package com.api_search.project.unitary.exception;
+
+import com.api_search.project.excepetion.AccountsExcept;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class AccountsExceptTest {
-    
+    @Test
+    void shouldCreateExceptionWithCorrectMessage() {
+        AccountsExcept exception = new AccountsExcept("not found");
+
+        assertEquals("Error in class Accounts: not found", exception.getMessage());
+    }
+
+    @Test
+    void shouldBeInstanceOfRuntimeException() {
+        AccountsExcept exception = new AccountsExcept("error");
+
+        assertInstanceOf(RuntimeException.class, exception);
+    }
 }
