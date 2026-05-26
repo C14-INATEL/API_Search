4c7571b2b5650a0eb165451562b0b1df2337390d
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-25 21:54:32 -0300

    feat: ✨ new resource, treatment of errors and throws of exception

--- src/main/java/com/api_search/project/service/UserService.java
+++ src/main/java/com/api_search/project/service/UserService.java
@@ -1,11 +1,10 @@
 package com.api_search.project.service;
 
 import com.api_search.project.entity.User;
+import com.api_search.project.excepetion.UserExcept;
 import com.api_search.project.repository.UserRepository;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
-
-import java.sql.Struct;
 import java.util.List;
 
 @Service
@@ -17,57 +16,110 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void save(User user){
-        if (user.getPassword() == null) {
-            throw new RuntimeException("PASSWORD IS NULL");
+    public void save(User user) throws UserExcept {
+        try{
+            if (user.getPassword() == null) {
+                throw new RuntimeException("PASSWORD IS NULL");
+            }
+
+            String hash = hashCrypt(user.getPassword());
+            user.setPassword(hash);
+
+            userRepository.save(user);
         }
-
-        String hash = hashCrypt(user.getPassword());
-        user.setPassword(hash);
-
-        userRepository.save(user);
-    }
-    public String hashCrypt(String password){
-        String passwordCrypt;
-
-        passwordCrypt = BCrypt.hashpw(password, BCrypt.gensalt());
-        return  passwordCrypt;
-    }
-
-    public Boolean checkPassword(String password){
-        String passwordCrypt = hashCrypt(password);
-        boolean isValid = BCrypt.checkpw(password,passwordCrypt);
-        if (isValid){
-            return true;
-        }else
+        catch (Exception e)
         {
-            return false;
+            throw new UserExcept(e.getMessage());
         }
     }
-    public User searchById(Integer id){
-        return userRepository.findById(id).orElse(null);
+    public String hashCrypt(String password) throws UserExcept{
+        try
+        {
+            String passwordCrypt;
+
+            passwordCrypt = BCrypt.hashpw(password, BCrypt.gensalt());
+            return  passwordCrypt;
+        }
+        catch (Exception e)
+        {
+            throw new UserExcept(e.getMessage());
+        }
     }
 
-    public List<User> searchAll(){
-        return userRepository.findAll();
+    public Boolean checkPassword(String password) throws UserExcept{
+        try
+        {
+            String passwordCrypt = hashCrypt(password);
+            boolean isValid = BCrypt.checkpw(password,passwordCrypt);
+            if (isValid){
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+        catch (Exception e){
+            throw new UserExcept(e.getMessage());
+        }
+    }
+    public User searchById(Integer id) throws UserExcept{
+        try {
+            return userRepository.findById(id).orElse(null);
+        }
+        catch (Exception e){
+            throw new UserExcept(e.getMessage());
+        }
     }
 
-    public boolean existsByid(Integer id) {
-        return userRepository.existsById(id);
+    public List<User> searchAll() throws UserExcept{
+        try {
+            return userRepository.findAll();
+        }
+        catch (Exception e){
+            throw new UserExcept(e.getMessage());
+        }
     }
 
-    public void deleteByid(Integer id){
-        userRepository.deleteById(id);
+    public boolean existsByid(Integer id) throws UserExcept{
+        try{
+            return userRepository.existsById(id);
+        }
+        catch (Exception e)
+        {
+            throw new UserExcept(e.getMessage());
+        }
     }
 
-    public void deleteALL(){
-        userRepository.deleteAll();
+    public void deleteByid(Integer id) throws UserExcept{
+        try{
+            userRepository.deleteById(id);
+        }
+        catch (Exception e)
+        {
+            throw new UserExcept(e.getMessage());
+        }
     }
 
-    public User update(Integer id, User userRequest) {
-        User existing = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not exist " + id));
-        existing.setName(userRequest.getName());
-        existing.setEmail(userRequest.getEmail());
-        return userRepository.save(existing);
+    public void deleteALL() throws UserExcept{
+        try
+        {
+            userRepository.deleteAll();
+        }
+        catch (Exception e){
+            throw new UserExcept(e.getMessage());
+        }
+    }
+
+    public User update(Integer id, User userRequest) throws UserExcept{
+        try
+        {
+            User existing = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not exist " + id));
+            existing.setName(userRequest.getName());
+            existing.setEmail(userRequest.getEmail());
+            return userRepository.save(existing);
+        }
+        catch (Exception e){
+            throw new UserExcept(e.getMessage());
+        }
     }
 }
