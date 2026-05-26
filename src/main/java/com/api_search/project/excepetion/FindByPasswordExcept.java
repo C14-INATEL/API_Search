a53583b3c834e255e7a66878a2ab257335514a82
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-25 21:52:18 -0300

    feat: ✨ new resource, treatment of errors and throws of exception

--- src/main/java/com/api_search/project/excepetion/FindByPasswordExcept.java
+++ src/main/java/com/api_search/project/excepetion/FindByPasswordExcept.java
@@ -1,5 +1,9 @@
 package com.api_search.project.excepetion;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class FindByPasswordExcept extends RuntimeException {
     public FindByPasswordExcept(String message) {
         super("Consult HIBP password Fail " + message);
