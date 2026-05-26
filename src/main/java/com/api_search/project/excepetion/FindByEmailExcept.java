35402ac027aafa8d28212822a9c13f7933522294
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-25 21:51:56 -0300

    feat: ✨ new resource, treatment of errors and throws of exception

--- src/main/java/com/api_search/project/excepetion/FindByEmailExcept.java
+++ src/main/java/com/api_search/project/excepetion/FindByEmailExcept.java
@@ -1,7 +1,11 @@
 package com.api_search.project.excepetion;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class FindByEmailExcept extends RuntimeException {
     public FindByEmailExcept(String message) {
-        super(message);
+        super("Error in client FindByEmail: " + message);
     }
 }
