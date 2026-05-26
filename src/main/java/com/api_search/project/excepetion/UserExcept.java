1725a349b0447073c8a2989df44a32308992c01f
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-25 21:53:03 -0300

    feat: ✨ new resource, treatment of errors and throws of exception

--- src/main/java/com/api_search/project/excepetion/UserExcept.java
+++ src/main/java/com/api_search/project/excepetion/UserExcept.java
@@ -1,7 +1,11 @@
 package com.api_search.project.excepetion;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class UserExcept extends RuntimeException {
-  public UserExcept(String message) {
-    super(message);
-  }
+    public UserExcept(String message) {
+        super("Error in class User " + message);
+    }
 }
