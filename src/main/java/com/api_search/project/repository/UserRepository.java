d03b1f5028c12efcf31682cceafdbc7ab6777e7b
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-05-31 12:58:42 -0300

    feat: ✨ adding new resource, of looking for user and password and verify if exist and return id to front end treat

--- src/main/java/com/api_search/project/repository/UserRepository.java
+++ src/main/java/com/api_search/project/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.api_search.project.repository;
 
 import com.api_search.project.entity.User;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
