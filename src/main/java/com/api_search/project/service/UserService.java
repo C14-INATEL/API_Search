62036f07b0fe933a90eb4d3698529f48002c5d8b
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-06-03 23:13:05 -0300

    fix: 🐛 correcting bug with special character in passowrd, add treat of case

--- src/main/java/com/api_search/project/service/UserService.java
+++ src/main/java/com/api_search/project/service/UserService.java
@@ -56,12 +56,10 @@ public class UserService {
 
     public Integer searchUserWithEmailPassword(String email, String password) throws UserExcept {
         try {
-            String passwordDecoded = java.net.URLDecoder.decode(password, "UTF-8");
-
             User user = userRepository.findByEmail(email)
                     .orElseThrow(() -> new RuntimeException("User not found"));
 
-            boolean validPassword = BCrypt.checkpw(passwordDecoded, user.getPassword());
+            boolean validPassword = BCrypt.checkpw(password, user.getPassword());
 
             if (!validPassword) {
                 throw new RuntimeException("Invalid Password");
