0d05269777025747233a4cda599d12eff418d8a2
LuanPirr3 <lu4n.engs0ft@gmail.com>
2026-06-07 15:40:15 -0300

    feat: ✨ adding new resource, adding rate limit api

--- src/main/java/com/api_search/project/ratelimit/RateLimitInterceptor.java
+++ src/main/java/com/api_search/project/ratelimit/RateLimitInterceptor.java
@@ -1,4 +1,36 @@
 package com.api_search.project.ratelimit;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class RateLimitInterceptor {
+@Component
+public class RateLimitInterceptor implements HandlerInterceptor {
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    private Bucket createBucket(){
+        return Bucket.builder()
+                .addLimit(Bandwidth.classic(5, Refill.intervally(5, Duration.ofSeconds(15))))
+                .build();
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        String ip = request.getRemoteAddr();
+        Bucket bucket = buckets.computeIfAbsent(ip, k -> createBucket());
+
+        if (bucket.tryConsume(1)){
+            return true;
+        }
+
+        response.setStatus(429);
+        response.getWriter().write("Muitas requisições. Tente novamente daqui alguns minutos");
+        return false;
+    }
 }
--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,4 +1,22 @@
 package config;
 
-public class WebConfig {
+import com.api_search.project.ratelimit.RateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api-search/users/save")
+                .addPathPatterns("/api-search/users/login")
+                .addPathPatterns("/api-search/accounts/user/")
+                .addPathPatterns("api-search/accounts/email/");
+    }
 }
